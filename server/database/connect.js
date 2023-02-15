5309b000a6ee9d9b530b1b07b4fc5f7e2809c449
Tan <davidtanpro1998@gmail.com>
2023-02-16 00:22:24 +0700

    finish

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,5 +1,6 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import ENV from "../config.js"
 
 async function connect() {
   const mongodb = await MongoMemoryServer.create();
@@ -7,7 +8,7 @@ async function connect() {
 
   mongoose.set("strictQuery", true);
 
-  const db = await mongoose.connect(getUri);
+  const db = await mongoose.connect(ENV.MONGO_DB_ATLAS);
   console.log("Database connected");
   return db;
 }
